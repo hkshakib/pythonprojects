7b67902f88f6f79bab8908932f904754dabf7758
hkshakib <humayunkibriashakib@gmail.com>
2023-07-04 10:55:23 +0600

    feat: Basic QRCode Generator implemented and added requirements.txt files

---
+++ QrCodeGenerator.py
@@ -0,0 +1,16 @@
+import qrcode
+import image
+
+qr = qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5,
+)
+
+data = "https://www.linkedin.com/in/hkshakib/"
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill="black", back_color="white")
+img.show()
+img.save("linkedin.png")
